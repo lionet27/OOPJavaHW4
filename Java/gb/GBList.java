7d2e4d1089275462a8a4a65c6849ef7eb6fac4cf
«OlgaSoboleva27» <osoboleva27@gmail.com>
2023-03-19 13:13:29 +0300

    add GBList

---
+++ Java/gb/GBList.java
@@ -0,0 +1,9 @@
+package gb;
+
+public interface GBList<T> extends Iterable<T> {
+    boolean add(T type);
+    void remove(int index);
+    T get(int index);
+    int size();
+    void update(int index, T type); 
+}
