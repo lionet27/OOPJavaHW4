3751f3c61f0714a5b6e161cbb0be56ff0b1b4443
«OlgaSoboleva27» <osoboleva27@gmail.com>
2023-03-19 13:20:16 +0300

    add NOde

---
+++ Java/gb/linkedlist/Node.java
@@ -0,0 +1,14 @@
+package gb.linkedlist;
+
+public  final class  Node<T> {
+    T item;
+    Node<T> next;
+    Node<T> prev;
+
+    Node(T item) {
+        this.item = item;
+        
+    }
+
+    
+}
